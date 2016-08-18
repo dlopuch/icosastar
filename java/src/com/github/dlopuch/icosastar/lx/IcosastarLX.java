a531b35dc09cccd7907a1796267401d279091a44
Dan Lopuch <dan@datahero.com>
2016-08-17 18:08:14 -0700

    Adding (icosa) beat detection to LXPerlin

--- java/src/com/github/dlopuch/icosastar/lx/IcosastarLX.java
+++ java/src/com/github/dlopuch/icosastar/lx/IcosastarLX.java
@@ -6,6 +6,7 @@ import com.github.dlopuch.icosastar.lx.model.IcosastarLXModelBuilder;
 import com.github.dlopuch.icosastar.lx.patterns.PerlinNoisePattern;
 import com.github.dlopuch.icosastar.lx.patterns.RainbowPattern;
 import com.github.dlopuch.icosastar.lx.patterns.RainbowSpreadPattern;
+import com.github.dlopuch.icosastar.signal.IcosaFFT;
 import com.github.dlopuch.icosastar.widgets.FrameRateCalculator;
 import heronarts.lx.output.FadecandyOutput;
 import heronarts.lx.pattern.LXPattern;
@@ -26,6 +27,8 @@ public class IcosastarLX extends PApplet {
   private P3LX lx;
   private IcosastarLXModel model;
 
+  private IcosaFFT icosaFft = new IcosaFFT(this);
+
   private FrameRateCalculator frc = new FrameRateCalculator(this, 3000);
 
   public void settings() {
@@ -37,7 +40,7 @@ public class IcosastarLX extends PApplet {
     lx = new P3LX(this, model);
 
     lx.setPatterns(new LXPattern[] {
-        new PerlinNoisePattern(lx, this),
+        new PerlinNoisePattern(lx, this, icosaFft),
         new RainbowPattern(lx),
         new RainbowSpreadPattern(lx),
     });
@@ -59,6 +62,7 @@ public class IcosastarLX extends PApplet {
     background(0x292929);
     // ...and everything else is handled by P3LX!
 
-    frc.draw();
+    //frc.draw();
+    icosaFft.forward();
   }
 }
--- java/src/com/github/dlopuch/icosastar/lx/patterns/PerlinNoisePattern.java
+++ java/src/com/github/dlopuch/icosastar/lx/patterns/PerlinNoisePattern.java
@@ -3,6 +3,8 @@ package com.github.dlopuch.icosastar.lx.patterns;
 
 import com.github.dlopuch.icosastar.effects.perlin_noise.PerlinNoiseExplorer;
 import com.github.dlopuch.icosastar.lx.model.AbstractIcosaLXModel;
+import com.github.dlopuch.icosastar.signal.IcosaFFT;
+import ddf.minim.analysis.BeatDetect;
 import heronarts.lx.LX;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.SawLFO;
@@ -18,6 +20,8 @@ import java.util.stream.Collectors;
 
 public class PerlinNoisePattern extends LXPattern {
 
+  private BeatDetect beat;
+
   private LXPerlinNoiseExplorer hueNoise;
   public final DiscreteParameter huePeriodMs = new DiscreteParameter("h period", 10000, 1000, 30000);
   private SawLFO hueOffset = new SawLFO(0, 360, huePeriodMs);
@@ -25,17 +29,17 @@ public class PerlinNoisePattern extends LXPattern {
   public final LXParameter hueSpeed;
   public final LXParameter hueXForm;
 
-  private PerlinNoiseExplorer brightnessBoostNoise;
+  private LXPerlinNoiseExplorer brightnessBoostNoise;
   private float brightnessBoostT = 0;
   private BasicParameter brightnessBoostDecay = new BasicParameter("bright decay", 0.97, 0.999, 0.80);
 
-  public PerlinNoisePattern(LX lx, PApplet p) {
+  public PerlinNoisePattern(LX lx, PApplet p, IcosaFFT fft) {
     super(lx);
+
+    this.beat = fft.beat;
 
     addModulator(hueOffset).start();
 
-
-    addParameter(brightnessBoostDecay);
 
     List<PVector> leds = this.model.getPoints().stream()
         .map(pt -> new PVector(pt.x, pt.y, pt.z))
@@ -47,20 +51,37 @@ public class PerlinNoisePattern extends LXPattern {
     addParameter(this.hueXForm = hueNoise.noiseXForm);
 
 
-//    this.brightnessBoostNoise = new PerlinNoiseExplorer(p, leds).setSpeed(hueNoiseSpeed.getValuef() * 2f);
-//    this.hueNoiseSpeed.addListener(hueSpeedParam -> brightnessBoostNoise.setSpeed(hueSpeedParam.getValuef() * 2f));
+    this.brightnessBoostNoise = new LXPerlinNoiseExplorer(p, this.model.getPoints(), "b");
+    addParameter(brightnessBoostDecay);
+    addParameter(brightnessBoostNoise.noiseSpeed);
+    addParameter(brightnessBoostNoise.noiseXForm);
+
 
     // initialize according to mapping
     ((AbstractIcosaLXModel) this.model).applyPresets(this);
+    brightnessBoostNoise.noiseSpeed.setValue(2.0 * this.hueSpeed.getValue());
+    brightnessBoostNoise.noiseXForm.setValue(0.5 * this.hueXForm.getValue());
   }
 
   public void run(double deltaMx) {
+    boolean isBrightnessBoost = beat.isKick();
+    if (isBrightnessBoost) {
+      brightnessBoostT = 1.0f;
+    } else if (brightnessBoostT > 0.05) {
+      brightnessBoostT *= brightnessBoostDecay.getValuef();
+    }
+
     for (LXPoint p : this.model.points) {
       colors[p.index] = LX.hsb(
           (hueOffset.getValuef() + 360 * hueNoise.getNoise(p.index)) % 360,
           100,
-          70
+          30 + (brightnessBoostT > 0.05 ? brightnessBoostT * 60 * brightnessBoostNoise.getNoise(p.index) : 0)
       );
+    }
+
+    if (beat.isSnare() && beat.isKick()) {
+      double newHue = (hueOffset.getValue() + Math.random() * 360 - 180) % 360;
+      hueOffset.setValue(newHue > 0 ? newHue : 360 + newHue);
     }
 
     hueNoise.step();
