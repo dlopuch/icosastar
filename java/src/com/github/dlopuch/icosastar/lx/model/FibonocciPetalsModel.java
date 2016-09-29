2dfa86e4283b987d04325b2b0f988255a35d360f
Dan Lopuch <dan@datahero.com>
2016-09-29 16:49:58 -0700

    Fixing mapping bugs and creating layout tester

--- java/src/com/github/dlopuch/icosastar/lx/model/FibonocciPetalsModel.java
+++ java/src/com/github/dlopuch/icosastar/lx/model/FibonocciPetalsModel.java
@@ -1,9 +1,6 @@
 package com.github.dlopuch.icosastar.lx.model;
 
-import com.github.dlopuch.icosastar.lx.patterns.LedSelectorPattern;
-import com.github.dlopuch.icosastar.lx.patterns.PerlinNoisePattern;
-import com.github.dlopuch.icosastar.lx.patterns.RainbowPattern;
-import com.github.dlopuch.icosastar.lx.patterns.RainbowSpreadPattern;
+import com.github.dlopuch.icosastar.lx.patterns.*;
 import com.github.dlopuch.icosastar.signal.IcosaFFT;
 import heronarts.lx.LX;
 import heronarts.lx.model.LXFixture;
@@ -12,9 +9,8 @@ import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.transform.LXVector;
 import processing.core.PApplet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Fibonocci Petals LED poster
@@ -25,15 +21,16 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
   // Coordinate system scale factor to get approximately in sync with other models
   private static final float ALPHA = 10;
 
-  public static class PetalPoint {
+  public static class Petal {
     public final LXVector point;
 
     private LXFixture cwSide;
     private LXFixture ccwSide;
+    private LXFixture allPoints;
 
-    private PetalPoint(double thetaRad, int numRotations) {
+    private Petal(double thetaRad, int numRotations) {
       // Set magnitude of vector based on numRotations
-      point = new LXVector(ALPHA * numRotations, 0, 0);
+      point = new LXVector(ALPHA * (numRotations + 1), 0, 0);
 
       // and add rotation
       point.rotate((float)thetaRad);
@@ -46,6 +43,52 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
     public LXFixture getCcwSide() {
       return ccwSide;
     }
+
+    public LXFixture getPoints() {
+      return allPoints;
+    }
+  }
+
+  public static class PetalSpiral implements LXFixture {
+    private final List<Petal> petals;
+    private final Set<LXPoint> allPoints;
+    private final Set<LXPoint> cwPoints;
+    private final Set<LXPoint> ccwPoints;
+
+    private PetalSpiral(Petal[] petals) {
+      this.petals = Arrays.asList(petals);
+
+      List<LXPoint> cwPointsList = this.petals.stream()
+          .flatMap(petal -> petal.cwSide.getPoints().stream())
+          .collect(Collectors.toList());
+
+      List<LXPoint> ccwPointsList = this.petals.stream()
+          .flatMap(petal -> petal.ccwSide.getPoints().stream())
+          .collect(Collectors.toList());
+
+      cwPoints = new HashSet<>(cwPointsList);
+      ccwPoints = new HashSet<>(ccwPointsList);
+
+      allPoints = new HashSet<>(cwPoints);
+      allPoints.addAll(ccwPoints);
+    }
+
+    @Override
+    public List<LXPoint> getPoints() {
+      return new ArrayList<>(allPoints);
+    }
+
+    public Set<LXPoint> getCwPoints() {
+      return cwPoints;
+    }
+
+    public Set<LXPoint> getCcwPoints() {
+      return ccwPoints;
+    }
+
+    public List<Petal> getPetals() {
+      return petals;
+    }
   }
 
   // Processing's bezierPoint() function
@@ -54,51 +97,51 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
     return a * t1 * t1 * t1 + 3.0F * b * t * t1 * t1 + 3.0F * c * t * t * t1 + d * t * t * t;
   }
 
-  private static PetalPoint[] petals = {
+  private static Petal[] petals = {
       // Fibonocci petals are derived by thetaInc = 2*PI * 1/PHI and continuing to spin that around
       // (ie first petal is 0*thetaInc, then 1*thetaInc, then 2*thetaInc, etc., all mod 2PI)
       // Here we've done that but thrown away everything that's >PI (ie using only "top half" of circle)
-      new PetalPoint(0.000000000, 0) , // 0
-      new PetalPoint(2.399892449, 0) , // 1
-      new PetalPoint(0.916677346, 1) , // 2
-      new PetalPoint(1.833354692, 2) , // 3
-      new PetalPoint(0.350139589, 3) , // 4
-      new PetalPoint(2.750032038, 3) , // 5
-      new PetalPoint(1.266816936, 4) , // 6
-      new PetalPoint(2.183494282, 5) , // 7
-      new PetalPoint(0.700279179, 6) , // 8
-      new PetalPoint(3.100171628, 6) , // 9
-      new PetalPoint(1.616956525, 7) , // 10
-      new PetalPoint(0.133741422, 8) , // 11
-      new PetalPoint(2.533633871, 8) , // 12
-      new PetalPoint(1.050418768, 9) , // 13
-      new PetalPoint(1.967096114, 10), // 14
-      new PetalPoint(0.483881012, 11), // 15
-      new PetalPoint(2.883773461, 11), // 16
-      new PetalPoint(1.400558358, 12), // 17
-      new PetalPoint(2.317235704, 13), // 18
-      new PetalPoint(0.834020601, 14), // 19
-      new PetalPoint(1.750697947, 15), // 20
-      new PetalPoint(0.267482845, 16), // 21
-      new PetalPoint(2.667375293, 16), // 22
-      new PetalPoint(1.184160191, 17), // 23
-      new PetalPoint(2.100837537, 18)  // 24
+      new Petal(0.000000000, 0) , // 0
+      new Petal(2.399892449, 0) , // 1
+      new Petal(0.916677346, 1) , // 2
+      new Petal(1.833354692, 2) , // 3
+      new Petal(0.350139589, 3) , // 4
+      new Petal(2.750032038, 3) , // 5
+      new Petal(1.266816936, 4) , // 6
+      new Petal(2.183494282, 5) , // 7
+      new Petal(0.700279179, 6) , // 8
+      new Petal(3.100171628, 6) , // 9
+      new Petal(1.616956525, 7) , // 10
+      new Petal(0.133741422, 8) , // 11
+      new Petal(2.533633871, 8) , // 12
+      new Petal(1.050418768, 9) , // 13
+      new Petal(1.967096114, 10), // 14
+      new Petal(0.483881012, 11), // 15
+      new Petal(2.883773461, 11), // 16
+      new Petal(1.400558358, 12), // 17
+      new Petal(2.317235704, 13), // 18
+      new Petal(0.834020601, 14), // 19
+//      new Petal(1.750697947, 15), // 20
+//      new Petal(0.267482845, 16), // 21
+//      new Petal(2.667375293, 16), // 22
+//      new Petal(1.184160191, 17), // 23
+//      new Petal(2.100837537, 18)  // 24
   };
 
   private static LXVector origin = new LXVector(0,0,0);
 
-  private static void addPetal(List<LXFixture> allFixtures, PetalPoint petal, int numLedsThere, int numLedsBack) {
+  private static void addPetal(List<LXFixture> allFixtures, Petal petal, int numLedsThere, int numLedsBack) {
     addPetal(allFixtures, petal, numLedsThere, numLedsBack, true);
   }
-  private static void addPetal(List<LXFixture> allFixtures, PetalPoint petal, int numLedsThere, int numLedsBack,
+  private static void addPetal(List<LXFixture> allFixtures, Petal petal, int numLedsThere, int numLedsBack,
                                boolean stripGoesCw) {
     LXVector tip = petal.point;
     LXVector originControl = origin.copy().lerp(tip, 0.4f);
     LXVector tipControlThere = tip.copy()
-        .rotate((stripGoesCw ? 1 : -1) * (float)Math.PI * 1.2f/2f)
+        .rotate((stripGoesCw ? -1 : 1) * (float)Math.PI * 1.2f/2f)
         .setMag(tip.mag() * 0.3f);
     LXVector tipControlBack = tip.copy()
-        .rotate((stripGoesCw ? -1 : 1) * (float)Math.PI * 1.2f/2f)
+        .rotate((stripGoesCw ? 1 : -1) * (float)Math.PI * 1.2f/2f)
         .setMag(tip.mag() * 0.3f);
 
     List<LXPoint> therePts = new ArrayList<>(numLedsThere);
@@ -110,8 +153,8 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
     }
 
     List<LXPoint> backPts = new ArrayList<>(numLedsThere);
-    for (int i=0; i<numLedsThere; i++) {
-      therePts.add(new LXPoint(
+    for (int i=0; i<numLedsBack; i++) {
+      backPts.add(new LXPoint(
           bezierPoint(tip.x, tip.x + tipControlBack.x, originControl.x, origin.x, (float)i / ((float)numLedsThere)),
           bezierPoint(tip.y, tip.y + tipControlBack.y, originControl.y, origin.y, (float)i / ((float)numLedsThere))
       ));
@@ -122,6 +165,10 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
 
     petal.cwSide  = stripGoesCw ? there : back;
     petal.ccwSide = stripGoesCw ? back  : there;
+
+    List<LXPoint> allPetalPoints = new ArrayList<>(therePts);
+    allPetalPoints.addAll(backPts);
+    petal.allPoints = () -> allPetalPoints;
 
     allFixtures.add(there);
     allFixtures.add(back);
@@ -161,9 +208,9 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
     addTail(allFixtures, tails, 8);
 
     // FC port 2
-    addPetal(allFixtures, petals[12], 13, 13);
-    addPetal(allFixtures, petals[10], 11, 13);
-    addTail(allFixtures, tails, 15);
+    addPetal(allFixtures, petals[12], 13, 13, false);
+    addPetal(allFixtures, petals[10], 11, 13, false);
+    addTail(allFixtures, tails, 14);
 
     // FC port 3
     addPetal(allFixtures, petals[11], 13, 13);
@@ -186,7 +233,97 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
     addPetal(allFixtures, petals[16], 16, 16);
     addTail(allFixtures, tails, 32);
 
-    return new FibonocciPetalsModel(allFixtures, hasGui);
+
+    // Okay.  LED's are mapped.
+    // Now lets define higher-order model attributes.
+
+
+    List<PetalSpiral> cwSpirals = new ArrayList();
+    cwSpirals.add(new PetalSpiral(new Petal[]{
+        petals[11],
+        petals[15],
+        petals[19],
+    }));
+
+    cwSpirals.add(new PetalSpiral(new Petal[]{
+        petals[0],
+        petals[4],
+        petals[8],
+        petals[13],
+        petals[17],
+    }));
+
+    cwSpirals.add(new PetalSpiral(new Petal[]{
+        petals[2],
+        petals[6],
+        petals[10],
+        petals[14],
+        petals[18],
+    }));
+
+    cwSpirals.add(new PetalSpiral(new Petal[]{
+        petals[3],
+        petals[7],
+        petals[12],
+        petals[16],
+    }));
+
+    cwSpirals.add(new PetalSpiral(new Petal[]{
+        petals[1],
+        petals[5],
+        petals[9],
+    }));
+
+    return new FibonocciPetalsModel(allFixtures, hasGui, Arrays.asList(petals), cwSpirals);
+  }
+
+  // END STATIC BUILDER
+  // -----------------------------
+
+  public final List<Petal> allPetals;
+  public final List<PetalSpiral> cwSpirals;
+
+  private FibonocciPetalsModel(List<LXFixture> allFixtures, boolean hasGui,
+                               List<Petal> allPetals, List<PetalSpiral> cwSpirals) {
+    super(allFixtures.toArray(new LXFixture[allFixtures.size()]), hasGui);
+
+    System.out.println("Starting up with fibonocci petals model");
+
+    this.allPetals = allPetals;
+    this.cwSpirals = cwSpirals;
+  }
+
+  /**
+   * Gets a cross section of petals from this layout
+   * @param spiralSelect Which cwSpiral to grab, or -1 for all
+   * @param petalSelect Which petal from the spiral to use, or -1 for all
+   * @return
+   */
+  public List<Petal> getPetals(int spiralSelect, int petalSelect) {
+    List<Petal> petals = new LinkedList<>();
+
+    if (spiralSelect >= 0) {
+      if (petalSelect >= 0) {
+        petals.add(cwSpirals.get(spiralSelect).getPetals().get(petalSelect));
+      } else {
+        petals.addAll(cwSpirals.get(spiralSelect).getPetals());
+      }
+    } else {
+      // all spirals
+      for (PetalSpiral spiral : cwSpirals) {
+        if (petalSelect >= 0) {
+          if (petalSelect >= spiral.getPetals().size()) {
+            continue;
+          }
+          petals.add(spiral.getPetals().get(petalSelect));
+
+        } else {
+          petals.addAll(spiral.getPetals());
+        }
+      }
+    }
+
+    return petals;
   }
 
   @Override
@@ -201,16 +338,11 @@ public class FibonocciPetalsModel extends AbstractIcosaLXModel {
 
     if (hasGui) {
       patterns.add(new LedSelectorPattern(lx));
+      patterns.add(new FibonocciPetalsLayoutTesterPattern(lx));
     }
 
     lx.setPatterns(patterns.toArray(new LXPattern[patterns.size()]));
     lx.goPattern(perlinNoise);
-  }
-
-  private FibonocciPetalsModel(List<LXFixture> allFixtures, boolean hasGui) {
-    super(allFixtures.toArray(new LXFixture[allFixtures.size()]), hasGui);
-
-    System.out.println("Starting up with fibonocci petals model");
   }
 
   @Override
